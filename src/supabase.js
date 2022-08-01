26c35883758203e5a68d72588db3af6f16801baf
Achraf TAJANI <achraf.tajani-ext@bnf.fr>
2022-08-01 16:55:24 +0200

    change env

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,9 @@
+// @ts-nocheck
+
 import { createClient } from '@supabase/supabase-js';
-// @ts-ignore
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL;
-// @ts-ignore
-const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY;
+
+const supabaseUrl = 'https://mwnclalpgbduuaaiwvxp.supabase.co';
+const supabaseAnonKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13bmNsYWxwZ2JkdXVhYWl3dnhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTkyNzk2OTQsImV4cCI6MTk3NDg1NTY5NH0.CAuUfi9EPzdi1kDtPSHGtVmCuaGATV1quPHeDZssnpo';
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
